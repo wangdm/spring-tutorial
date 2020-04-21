9ea344ee6435a7a10c28d5dd619baa63840cdaf0
wangdm <Wang411151499>
2020-04-21 22:53:57 +0800

    rename main class to Application

--- spring-security-tutorial/src/main/java/com/wangdm/tutorial/spring/security/Application.java
+++ spring-security-tutorial/src/main/java/com/wangdm/tutorial/spring/security/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityTutorial {
+public class Application {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(SpringSecurityTutorial.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
